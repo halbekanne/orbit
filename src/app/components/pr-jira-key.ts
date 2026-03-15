2d1c7e8d676746fd3345a3b75cccd0a9f3bb67e5
Dominik Halfkann <dominik.halfkann@mailbox.org>
2026-03-15 18:54:49 +0100

    feat: add extractJiraKey utility

---
+++ src/app/components/pr-jira-key.ts
@@ -0,0 +1,11 @@
+import { PullRequest } from '../models/work-item.model';
+
+const JIRA_KEY_PATTERN = /[A-Z]+-\d+/;
+
+export function extractJiraKey(pr: PullRequest): string | null {
+  return (
+    JIRA_KEY_PATTERN.exec(pr.fromRef.displayId)?.[0] ??
+    JIRA_KEY_PATTERN.exec(pr.title)?.[0] ??
+    null
+  );
+}
